6f051ac7217c916810a0079d70e586fef4bbdd0a
Abdullah <muhammad.abdullah@gmail.com>
2024-09-13 14:38:16 +0500

    TEst controller added to command services

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+
+        public PlatformsController()
+        {
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("Inbound POST # Command Service");
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
